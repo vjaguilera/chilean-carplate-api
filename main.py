ec1566a6203bd15cfc637e4a180298245cb1bd14
vjaguilera <vjaguilera@uc.cl>
2023-03-01 11:25:12 -0300

    :hammer: remove loggin level from uvicorn initializer

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ async def get_format_message(plate):
     return msg
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=8000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=8000))
